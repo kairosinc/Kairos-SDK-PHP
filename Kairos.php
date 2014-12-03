ce4d6ae0a77758853c48abdeef7b9962d6e05914
Eric <eric@kairos.com>
2014-12-02 23:33:42 -0500

    Pushing initial draft (in progress)

---
+++ Kairos.php
@@ -0,0 +1,436 @@
+<?php
+
+/* 
+ authored by Eric Turner 
+ Copyright 2014 Kairos AR INC
+ */
+
+
+class Kairos {
+
+
+
+ 	protected $hostname;
+ 	protected $app_id;
+ 	protected $api_key;
+
+
+
+	function __construct($app_id, $api_key) {
+
+       $this->hostname = 'http://api.kairos.com/';
+       $this->app_id = $app_id;
+       $this->api_key = $api_key;
+   	}
+
+
+
+   	private function imageDataFromFilePath($path)
+	{
+
+		$base64 = "";
+
+        if($fp = fopen($path,"rb", 0)) 
+        { 
+            $image = fread($fp,filesize($path)); 
+            fclose($fp); 
+            $base64 = chunk_split(base64_encode($image)); 	   
+		}
+
+		return $base64;
+	}
+
+
+
+
+	private function authenticationProvided()
+	{
+		if(count($this->api_key) > 0 && count($this->app_id) > 0)
+		{
+			return true;
+		}
+
+		return false;
+	}
+
+
+
+
+
+
+	public function enrollImageWithPath($image_path, $gallery_id, $subject_id, $options = array())
+	{
+
+		$image_data = $this->imageDataFromFilePath($image_path);
+
+		$response = $this->enrollImageWithData($image_data, $gallery_id, $subject_id, $options);
+
+		return $response;
+	}
+
+
+
+
+
+
+	public function enrollImageWithData($image_data, $gallery_id, $subject_id, $options = array())
+	{
+
+		if($this->authenticationProvided() == false)
+		{
+			return 'set your app_id and api_key before calling this method';
+		}
+
+    	$request_params = array(
+    							"image"        => $image_data, 
+    							"gallery_name" => $gallery_id, 
+    							"subject_id"   => $subject_id );
+
+    	// add options if provided
+    	if(count($options) > 0)
+    	{
+    		array_push($request_params, $options);
+    	}
+
+    	// build request string
+    	$request = json_encode($request_params);
+
+    	try
+    	{
+
+    		$ch = curl_init();
+
+    		curl_setopt($ch, CURLOPT_URL,            $this->hostname . "enroll" );
+    		curl_setopt($ch, CURLOPT_CUSTOMREQUEST,  "POST");
+    		curl_setopt($ch, CURLOPT_POSTFIELDS,     $request);
+    		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    		curl_setopt($ch, CURLOPT_HTTPHEADER, 
+        		array(
+          			'Content-Type: application/json',
+          			'Content-Length: ' . strlen($request),
+          			'app_id: ' . $this->app_id,
+          			'app_key: '. $this->api_key)
+        		);
+
+    		$response = curl_exec($ch);
+
+    		curl_close($ch);
+
+    	}
+    	catch(Exception $ex)
+  		{
+       		return 'there was a problem';
+    	}
+
+    	return $response;
+	}
+
+
+
+
+
+
+	public function detectWithImagePath($image_path, $options = array())
+	{
+
+		$image_data = $this->imageDataFromFilePath($image_path);
+
+		$response = $this->detectImageWithData($image_data, $options);
+
+		return $response;
+	}
+
+
+
+
+
+
+	public function detectWithImageData($image_data, $options = array())
+	{
+
+		if($this->authenticationProvided() == false)
+		{
+			return 'set your app_id and api_key before calling this method';
+		}
+
+    	$request_params = array(
+    							"image"  =>  $image_data
+    							);
+
+    	// add options if provided
+    	if(count($options) > 0)
+    	{
+    		array_push($request_params, $options);
+    	}
+
+    	// build request string
+    	$request = json_encode($request_params);
+
+    	try
+    	{
+
+    		$ch = curl_init();
+
+    		curl_setopt($ch, CURLOPT_URL,            $this->hostname . "detect" );
+    		curl_setopt($ch, CURLOPT_CUSTOMREQUEST,  "POST");
+    		curl_setopt($ch, CURLOPT_POSTFIELDS,     $request);
+    		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    		curl_setopt($ch, CURLOPT_HTTPHEADER, 
+        		array(
+          			'Content-Type: application/json',
+          			'Content-Length: ' . strlen($request),
+          			'app_id: ' . $this->app_id,
+          			'app_key: '. $this->api_key)
+        		);
+
+    		$response = curl_exec($ch);
+
+    		curl_close($ch);
+
+    	}
+    	catch(Exception $ex)
+  		{
+       		return 'there was a problem';
+    	}
+
+    	return $response;
+
+	}
+
+
+
+
+	public function recognizeWithImagePath($image_path, $gallery_id, $options = array())
+	{
+
+		$image_data = $this->imageDataFromFilePath($image_path);
+
+		$response = $this->recognizeImageWithData($image_data, $gallery_id, $options);
+
+		return $response;
+	}
+
+
+
+
+
+	public function recognizeWithImageData($image_data, $gallery_id, $options = array())
+	{
+
+		if($this->authenticationProvided() == false)
+		{
+			return 'set your app_id and api_key before calling this method';
+		}
+
+    	$request_params = array(
+    							"image"        => $image_data, 
+    							"gallery_name" => $gallery_id, 
+    							);
+
+    	// add options if provided
+    	if(count($options) > 0)
+    	{
+    		array_push($request_params, $options);
+    	}
+
+    	// build request string
+    	$request = json_encode($request_params);
+
+    	try
+    	{
+
+    		$ch = curl_init();
+
+    		curl_setopt($ch, CURLOPT_URL,            $this->hostname . "recognize" );
+    		curl_setopt($ch, CURLOPT_CUSTOMREQUEST,  "POST");
+    		curl_setopt($ch, CURLOPT_POSTFIELDS,     $request);
+    		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    		curl_setopt($ch, CURLOPT_HTTPHEADER, 
+        		array(
+          			'Content-Type: application/json',
+          			'Content-Length: ' . strlen($request),
+          			'app_id: ' . $this->app_id,
+          			'app_key: '. $this->api_key)
+        		);
+
+    		$response = curl_exec($ch);
+
+    		curl_close($ch);
+
+    	}
+    	catch(Exception $ex)
+  		{
+       		return 'there was a problem';
+    	}
+
+    	return $response;
+	}
+
+
+
+	public function listGalleries($options = array())
+	{
+
+		if($this->authenticationProvided() == false)
+		{
+			return 'set your app_id and api_key before calling this method';
+		}
+
+    	$request_params = array();
+
+    	// add options if provided
+    	if(count($options) > 0)
+    	{
+    		array_push($request_params, $options);
+    	}
+
+    	// build request string
+    	$request = json_encode($request_params);
+
+    	try
+    	{
+
+    		$ch = curl_init();
+
+    		curl_setopt($ch, CURLOPT_URL,            $this->hostname . "gallery/list" );
+    		curl_setopt($ch, CURLOPT_CUSTOMREQUEST,  "POST");
+    		curl_setopt($ch, CURLOPT_POSTFIELDS,     $request);
+    		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    		curl_setopt($ch, CURLOPT_HTTPHEADER, 
+        		array(
+          			'Content-Type: application/json',
+          			'Content-Length: ' . strlen($request),
+          			'app_id: ' . $this->app_id,
+          			'app_key: '. $this->api_key)
+        		);
+
+    		$response = curl_exec($ch);
+
+    		curl_close($ch);
+
+    	}
+    	catch(Exception $ex)
+  		{
+       		return 'there was a problem';
+    	}
+
+    	return $response;
+	}
+
+
+
+
+	public function listSubjectsForGallery($gallery_id, $options = array())
+	{
+
+
+		if($this->authenticationProvided() == false)
+		{
+			return 'set your app_id and api_key before calling this method';
+		}
+
+    	$request_params = array(
+    							"gallery_name" => $gallery_id, 
+    							);
+
+    	// add options if provided
+    	if(count($options) > 0)
+    	{
+    		array_push($request_params, $options);
+    	}
+
+    	// build request string
+    	$request = json_encode($request_params);
+
+    	try
+    	{
+
+    		$ch = curl_init();
+
+    		curl_setopt($ch, CURLOPT_URL,            $this->hostname . "gallery/view" );
+    		curl_setopt($ch, CURLOPT_CUSTOMREQUEST,  "POST");
+    		curl_setopt($ch, CURLOPT_POSTFIELDS,     $request);
+    		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    		curl_setopt($ch, CURLOPT_HTTPHEADER, 
+        		array(
+          			'Content-Type: application/json',
+          			'Content-Length: ' . strlen($request),
+          			'app_id: ' . $this->app_id,
+          			'app_key: '. $this->api_key)
+        		);
+
+    		$response = curl_exec($ch);
+
+    		curl_close($ch);
+
+    	}
+    	catch(Exception $ex)
+  		{
+       		return 'there was a problem';
+    	}
+
+    	return $response;
+
+	}
+
+	public function removeSubjectFromGallery($subject_id, $gallery_id, $options = array())
+	{
+
+		if($this->authenticationProvided() == false)
+		{
+			return 'set your app_id and api_key before calling this method';
+		}
+
+    	$request_params = array(
+    							"gallery_name" => $gallery_id, 
+    							);
+
+    	// add options if provided
+    	if(count($options) > 0)
+    	{
+    		array_push($request_params, $options);
+    	}
+
+    	// build request string
+    	$request = json_encode($request_params);
+
+    	try
+    	{
+
+    		$ch = curl_init();
+
+    		curl_setopt($ch, CURLOPT_URL,            $this->hostname . "gallery/remove_subject" );
+    		curl_setopt($ch, CURLOPT_CUSTOMREQUEST,  "POST");
+    		curl_setopt($ch, CURLOPT_POSTFIELDS,     $request);
+    		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    		curl_setopt($ch, CURLOPT_HTTPHEADER, 
+        		array(
+          			'Content-Type: application/json',
+          			'Content-Length: ' . strlen($request),
+          			'app_id: ' . $this->app_id,
+          			'app_key: '. $this->api_key)
+        		);
+
+    		$response = curl_exec($ch);
+
+    		curl_close($ch);
+
+    	}
+    	catch(Exception $ex)
+  		{
+       		return 'there was a problem';
+    	}
+
+    	return $response;
+	}
+
+
+
+
+
+	
+
+}
+
+
+?>
--- README.md
+++ README.md
@@ -1,4 +1,171 @@
-Kairos-SDK-PHP
+Kairos SDK (PHP)
 ==============
 
-The PHP wrapper for the Kairos API  https://developer.kairos.com/
+Kairos is the easist way add **Face-Recognition** to your web applications. Our API provides a full-featured and robust Face-Recognition backend, right out of the box. This is the PHP wrapper for the [Kairos Facial Recognition API](https://www.kairos.com). The package includes a **class** _(Kairos.php)_ you can use as an easy client to the API. Continue reading to learn how to integrate Kairos into your web application.
+
+_Thanks to contributions by some of our customers, we also have [Ruby](https://github.com/kany/kairos-api) and [.NET](https://github.com/humbywan/Kairos.Net) wrappers available. Also see our [iOS SDK](https://github.com/kairosinc/Kairos-SDK-iOS)._
+
+## What You'll Need
+* An environment configured to run PHP scripts.
+
+
+
+---
+
+
+
+## How to Do a Quick Demo
+If you just want to do a quick test run, open the **example script** that is included with the SDK and follow these steps:
+
+1. [Create your free developer account](https://www.kairos.com/signup)
+2. Log into the Kairos Developer Dashboard
+3. Create an application and copy your **App Id** & **App Key**
+3. Paste them into the constructor method in example.php 
+4. Open the script in your **browser** and it will run.
+
+
+
+---
+
+
+## How to Install Kairos in Your Own Web App
+
+1. [Create your free Kairos developer account](https://www.kairos.com/signup) if you don't already have one.
+2. Log into the [dashboard](https://www.kairos.com/login) and create a new app.
+3. Copy your **App ID** & **App Key** (you'll need them later).
+4. [Download](https://github.com/kairosinc/Kairos-SDK-PHP) the SDK and unzip the package.
+5. Open the folder named **Kairos-SDK-PHP** containing the Kairos.php class.
+6. Place the wrapper class _(Kairos.php)_ somewhere within your application.
+7. Include Kairos.php where needed in your web application project.
+
+
+```
+ include("Kairos.php");
+```
+
+## Authenticate Once
+
+Before you can make API calls you'll need to pass Kairos your credentials **App Id** and **App Key** (You only need to do this once). Paste your App Id and App Key into the constructor method like so:
+
+```
+// instantiates a new instance
+$app_id  = 'arq3241';
+$api_key = 's82nrh2ns92nse2ha712net9rk3nter3';
+$Kairos  = new Kairos($app_id, $api_key);
+```
+
+
+
+## Enroll an Image Using a File Path
+
+The **Enroll** method **registers a face for later recognitions**. Here's an example of enrolling a face (subject) using a method that accepts an absolute path to an image file in your file system, and enrolls it as a new subject into your specified gallery:    
+
+```
+$path       = '/images/myphotos/myphoto123.png';
+$subject_id = 'eric';
+$gallery_id = 'friends1';
+$response   = $Kairos->enrollImageWithPath($path, $subject_id, $gallery_id);
+```
+
+
+
+## Enroll an Image Using Data
+
+The **Enroll** method **registers a face for later recognitions**. Here's an example of enrolling a face (subject) using a method that accepts image data in base64 format, and enrolls it as a new subject into your specified gallery:    
+
+```
+$base64_data = 'iVBORw0KGgoAAA ... ABJRU5ErkJggg==\r\n';
+$subject_id  = 'eric';
+$gallery_id  = 'friends1';
+$response    = $Kairos->enrollImageWithData($base64_data, $subject_id, $gallery_id);
+```
+
+
+
+## Recognize an Image Using a File Path
+
+The **Recognize** method takes an image of a subject and **attempts to match it against a given gallery of previously-enrolled subjects**. Here's an example of recognizing a subject using a method that accepts an absolute path to an image file in your file system, sends it to the API, and returns a match and confidence value:    
+
+```
+$path       = '/images/myphotos/myphoto123.png';
+$response   = $Kairos->recognizeImageWithPath($path);
+```
+
+
+
+## Recognize an Image Using Data
+
+The **Recognize** method takes an image of a subject and **attempts to match it against a given gallery of previously-enrolled subjects**. Here's an example of recognizing a subject using a method that accepts image data in base64 format, sends it to the API, and returns a match and confidence value:    
+
+```
+$base64_data  = 'iVBORw0KGgoAAA ... ABJRU5ErkJggg==\r\n';
+$response     = $Kairos->recognizeImageWithData($base64_data);
+```
+
+
+    
+    
+## Detect Image Attributes Using a File Path
+
+The **Detect** method takes an image of a subject and **returns various attributes pertaining to the face features**. The detect methods also accept an optional 'selector' parameter, allowing you to tweak the scope of the response ([see docs](https://www.kairos.com/docs) for more info on the detect selector). Here's an example of using detect via method that accepts a path to an image file, sends it to the API, and returns face attributes:    
+
+```
+$path       = '/images/myphotos/myphoto123.png';
+$response   = $Kairos->detectImageWithPath($path);
+```
+
+    
+## Detect Image Attributes Using Image Data
+
+The **Detect** method takes an image of a subject and **returns various attributes pertaining to the face features**. The detect methods also accept an optional 'selector' parameter, allowing you to tweak the scope of the response ([see docs](https://www.kairos.com/docs) for more info on the detect selector). Here's an example of using detect via method that accepts image data in base64 format, sends it to the API, and returns face attributes:    
+
+```
+$base64_data  = 'iVBORw0KGgoAAA ... ABJRU5ErkJggg==\r\n';
+$response     = $Kairos->detectImageWithData($base64_data);
+```
+
+
+    
+## List Your Galleries
+
+This method returns a list of all galleries you've created:
+
+```
+$response = $Kairos->listGalleries();
+```
+
+## List Your Subjects
+
+This method returns a list of all subjects for a given gallery:
+
+```
+$gallery_id = 'friends1';
+$response = $Kairos->listSubjectsForGallery($gallery_id);
+```
+
+## Remove a Subject
+
+This method removes a subject from given gallery:
+
+```
+$subject_id = 'dave';
+$response = $Kairos->removeSubjectFromGallery($subject_id);
+```
+
+    
+    
+## Optional Parameters
+
+Many of the Kairos API methods expose optional parameters for customizing the api response to fit your use-case. Here is an example of sending custom options in the detect call to limit the response to attributes pertaining to the eyes:
+
+```
+$path       = '/images/myphotos/myphoto123.png';
+$options    = array('selector' => 'EYES');
+$response   = $Kairos->detectImageWithPath($path, $options);
+```
+
+
+[![Stack Share](http://img.shields.io/badge/tech-stack-0690fa.svg?style=flat)](http://stackshare.io/kairos-api/kairos-facial-recognition-api)
+
+##Support 
+Have an issue? [Contact us](mailto:api@kairos.com) or [create an issue on GitHub](https://github.com/kairosinc/Kairos-SDK-PHP)
